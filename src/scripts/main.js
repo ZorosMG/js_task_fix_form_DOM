d3ff772261ae77849f7357067b1e00111736b67a
Denys <zorosmg5@mail.com>
2025-01-15 12:46:20 +0200

    add solution1

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -7,9 +7,9 @@ inputs.forEach((input) => {
 
   label.classList.add('field-label');
   label.setAttribute('for', input.id);
-  label.textContent = input.name.charAt(0).toUpperCase() + input.name.slise(1);
+  label.textContent = input.name.charAt(0).toUpperCase() + input.name.slice(1);
 
-  input.plaseHolder = input.name.charAt(0).toUpperCase() + input.name.slice(1);
+  input.plaseholder = input.name.charAt(0).toUpperCase() + input.name.slice(1);
 
   input.parentElement.appendChild(label);
 });
